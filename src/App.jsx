5b4869cf4894814941904eb9038913faf6653613
Vaibhav Negi <vaibhavnegi240@gmail.com>
2026-08-17 16:51:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function App(){
+  const [list,setList] = useState([]);
+const [notetxt,setNotetxt] = useState("");
+const [error,setError] = useState("");
+useEffect(() =>{
+  const saved = localStorage.getItem("key");
+  if(saved){
+    setList(JSON.parse(saved))
+  }
+})
+function handleupdate(e){
+  e.preventDefault();
+  if(notetxt ===""){
+    setError("Pls write something")
+    return;
+  }
+  setError("");
+  setNotetxt("");
+  const Newarray = ([...list,notetxt]);
+  setList(Newarray);
+  saveStorage(Newarray);
+
+
+
+
+}
+function saveStorage(updatedarray){
+  localStorage.setItem("key", JSON.stringify(updatedarray))
+}
+function deletebtn(indexTodelete){
+  const updatedarray = (list.filter((note,index)=>(
+    index !== indexTodelete
+   
+    
+
+  )))
+   setList(updatedarray)
+   saveStorage(updatedarray)
+  
+
+}
+return(
+  <>
+  <h1>Note app</h1>
+  <form onSubmit={handleupdate}>
+    <input type="text" placeholder="Write your note" value={notetxt} onChange={(e) =>setNotetxt(e.target.value)} />
+    {error && <p style={{color:"red"}}>{error}</p>}
+      {list.map((note,index) =>
+  <p key ={index}>{note}<button type="button" onClick={() => deletebtn(index)}>Delete</button></p>
+
+  )}
+
+    
+    
+  </form>
+  </>
+)
+
+}
+export default App;
+
